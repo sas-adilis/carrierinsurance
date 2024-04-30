4b83ac59cb606718d66adaaca67eba1458e72a98
sas-adilis <contact@adilis.fr>
2024-04-30 07:59:40 +0200

    run php-cs-fixer

---
+++ views/js/checkout.js
@@ -0,0 +1,12 @@
+console.log(prestashop.selectors.checkout);
+/*prestashop.on('updatedDeliveryForm', (params) => {
+    console.log('passe', params);
+    if (
+        typeof  params.deliveryOption !== 'undefined' &&
+        typeof  params.deliveryOption.prevObject !== 'undefined' &&
+        typeof  params.deliveryOption.prevObject[0] !== 'undefined' &&
+        params.deliveryOption.prevObject[0].getAttribute('id') === 'carrier_insurance'
+    ) {
+        //document.location.reload();
+    }
+});*/
